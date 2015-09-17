e10da6c13ab1b5e24434506eb958b4bbb22f5f90
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-09-17 17:10:14 +0100

    Code refactor

--- db/migrate/20150914142448_add_users_to_links.rb
+++ db/migrate/20150914142448_add_users_to_links.rb
@@ -1,5 +1,5 @@
 class AddUsersToLinks < ActiveRecord::Migration
-  def change
-    add_reference :links, :user, index: true, foreign_key: true
-  end
+  # def change
+  #   add_reference :links, :user, index: true, foreign_key: true
+  # end
 end
